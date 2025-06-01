3a9f379e61aaf274aa2597fcb486523368d329a2
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2025-06-01 22:48:09 +0200

    feat: lets go

--- src/functions/ProcessNewsletter.ts
+++ src/functions/ProcessNewsletter.ts
@@ -1,6 +1,17 @@
 import { app, InvocationContext } from "@azure/functions";
+import nodemailer from "nodemailer";
 
 export async function ProcessNewsletter(queueItem: any, context: InvocationContext): Promise<void> {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "maddison53@ethereal.email",
+            pass: "jn7jnAPss4f63QBp6D",
+        },
+    });
+
     context.log("Service Bus queue message received:", queueItem, typeof queueItem);
 
     const emailData = typeof queueItem === "string" ? JSON.parse(queueItem) : queueItem;
