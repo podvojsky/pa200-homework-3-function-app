08a939b7e2aef2a534db19edfe4e03c05e82271f
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2025-06-02 09:21:27 +0200

    feat: lets go

--- src/functions/ProcessNewsletter.ts
+++ src/functions/ProcessNewsletter.ts
@@ -1,5 +1,5 @@
 import { app, InvocationContext } from "@azure/functions";
-import sgMail from "@sendgrid/mail";
+import * as sgMail from "@sendgrid/mail";
 
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || "";
 if (!SENDGRID_API_KEY) {
